9d3ea0a2975a68f491c8a5065a6efbf162e7d785
BharaviPatil1911 <bhairavipatil9527@gmail.com>
2024-09-07 01:01:13 +0530

    Add files via upload

---
+++ LogicBuilding/Assignment Day 3/snippet/IncrementDecrement.java
@@ -0,0 +1,13 @@
+//Snippet 6
+
+ public class IncrementDecrement { 
+ public static void main(String[] args) { 
+ int x = 5; 
+ int y = ++x - x-- + --x + x++; 
+ System.out.println(y); 
+ } 
+} 
+
+Output=
+8
+
---
+++ LogicBuilding/Assignment Day 3/snippet/WhileLoopBreak.java
@@ -0,0 +1,16 @@
+//Snippet 3
+
+public class WhileLoopBreak { 
+ public static void main(String[] args) { 
+ int count = 0; 
+ while (count < 5) { 
+ System.out.print(count + " "); 
+ count++; 
+ if (count == 3) break; 
+ } 
+ System.out.println(count); 
+ } 
+} 
+
+Output=
+0 1 2 3
